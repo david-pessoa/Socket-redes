2041ed0605f3b4fa942d56dc09436500f7273c8b
david-pessoa <pessoa.dvlbp@gmail.com>
2024-04-02 21:54:53 -0300

    cria ifs do jogo

--- ClientTCP.py
+++ ClientTCP.py
@@ -4,7 +4,9 @@
 # David Varão Lima Bentes Pessoa  10402647
 # Pedro Nomura
 
-import socket #importa modulo socket
+import socket
+
+from numpy import integer #importa modulo socket
 
 TCP_IP = '192.168.0.5' # endereço IP do cliente
 TCP_PORTA = 3225      # porta disponibilizada pelo servidor
@@ -27,18 +29,22 @@ while True:
     print("3 Tesoura")
 
     cliente_choice = input("Escolha: ") # Cliente insere mensagem
+    while not isinstance(cliente_choice, int) or cliente_choice != "QUIT":
+        print("Insira um número de 1 a 3 ou QUIT para sair")
+        cliente_choice = input("Escolha: ") # Cliente insere mensagem
     if cliente_choice == "QUIT": #Se o cliente digitou QUIT sai do chat
+        print("Encerrando conexão.....")
         break
     cliente_choice = int(cliente_choice)
     
     cliente.send(cliente_choice.encode('UTF-8')) # Mensagem é enviada ao servidor
-
-
     
     # recebe dados do servidor 
     data, addr = cliente.recvfrom(1024) #Cliente recebe resposta do servidor
     server_choice = data.decode("UTF-8") #Decodifica mensagem
-    if data == "QUIT": # Se a resposta do servidor foi QUIT sai do chat
+    if server_choice == "QUIT": # Se a resposta do servidor foi QUIT sai do chat
+        print("Seu oponente desistiu da partida")
+        print("Jogo encerrado.")
         break
     server_choice = int(server_choice)
 
@@ -61,19 +67,53 @@ while True:
         print("Oponente: Tesoura")
         print("Você ganhou!")
         placar[0] += 1
+    
+    # Papel Pedra
+    elif cliente_choice == 2 and server_choice == 1:
+        print("\n\n Você: Papel")
+        print("Oponente: Pedra")
+        print("Você ganhou!")
+        placar[0] += 1
+    
+    # Papel Papel
+    elif cliente_choice == 2 and server_choice == 2:
+        print("\n\n Você: Papel")
+        print("Oponente: Papel")
+        print("Empate")
+    
+    # Papel Tesoura
+    elif cliente_choice == 2 and server_choice == 3:
+        print("\n\n Você: Papel")
+        print("Oponente: Tesoura")
+        print("Você perdeu!")
+        placar[1] += 1
+    
+    # Tesoura Pedra
+    elif cliente_choice == 3 and server_choice == 1:
+        print("\n\n Você: Tesoura")
+        print("Oponente: Pedra")
+        print("Você perdeu!")
+        placar[1] += 1
+    
+    # Tesoura Papel
+    elif cliente_choice == 3 and server_choice == 2:
+        print("\n\n Você: Tesoura")
+        print("Oponente: Papel")
+        print("Você ganhou!")
+        placar[0] += 1
+    
+    # Tesoura Tesoura
+    elif cliente_choice == 3 and server_choice == 3:
+        print("\n\n Você: Tesoura")
+        print("Oponente: Tesoura")
+        print("Empate")
 
-
+    else:
+        print("Houve algum erro")
+        print("Encerrando conexão.....")
+        break
     #TODO: Continuar ifs para o cliente e servidor
     #TODO: Testar código
-    
-
-    # Papel Pedra
-    # Papel Papel
-    # Papel Tesoura
-
-    # Tesoura Pedra
-    # Tesoura Papel
-    # Tesoura Tesoura
 
 
 
--- ServerTCP.py
+++ ServerTCP.py
@@ -29,16 +29,92 @@ print ('Endereço conectado:', addr) #Exibe IP da máquina conectada
 while 1:
     show_placar(placar)
     print("Escolha entre:")
+    print("1 Pedra")
+    print("2 Papel")
+    print("3 Tesoura")
+
+    server_choice = input("Escolha: ") # Servidor insere mensagem
+    while not isinstance(server_choice, int) or server_choice != "QUIT":
+        print("Insira um número de 1 a 3 ou QUIT para sair")
+        server_choice = input("Escolha: ") # Servidor insere mensagem
+    if server_choice == "QUIT": #Se o servidor digitou QUIT sai do chat
+        print("Encerrando conexão.....")
+        break
+
+    server_choice = int(server_choice)
+
     #dados retidados da mensagem recebida
-    data = conn.recv(TAMANHO_BUFFER).decode("UTF-8") #Recebe resposta do cliente
-    if data: 
-        print ("Cliente:", data)
-        if data != "QUIT":
-            answer = input("Servidor: ")
-            conn.send(answer.encode("UTF-8"))  # envia dados recebidos em letra maiuscula
-            if answer == "QUIT":
-                break
+    cliente_choice = conn.recv(TAMANHO_BUFFER).decode("UTF-8") #Recebe resposta do cliente
+    if cliente_choice == "QUIT":
+        print("Seu oponente desistiu da partida")
+        print("Jogo encerrado.")
+    else:
+        
+        conn.send(server_choice.encode("UTF-8"))  # envia dados recebidos em letra maiuscula
+
+        # Pedra Pedra
+        if cliente_choice == 1 and server_choice == 1:
+            print("\n\n Você: Pedra")
+            print("Oponente: Pedra")
+            print("Empate")
+
+        # Papel Pedra
+        elif cliente_choice == 1 and server_choice == 2:
+            print("\n\n Você: Papel")
+            print("Oponente: Pedra")
+            print("Você ganhou!")
+            placar[1] += 1
+
+        # Tesoura Pedra
+        elif cliente_choice == 1 and server_choice == 3:
+            print("\n\n Você: Tesoura")
+            print("Oponente: Pedra")
+            print("Você perdeu!")
+            placar[0] += 1
+
+        # Pedra Papel
+        elif cliente_choice == 2 and server_choice == 1:
+            print("\n\n Você: Pedra")
+            print("Oponente: Papel")
+            print("Você perdeu!")
+            placar[0] += 1
+
+        # Papel Papel
+        elif cliente_choice == 2 and server_choice == 2:
+            print("\n\n Você: Papel")
+            print("Oponente: Papel")
+            print("Empate")
+
+        # Tesoura Papel
+        elif cliente_choice == 2 and server_choice == 3:
+            print("\n\n Você: Tesoura")
+            print("Oponente: Papel")
+            print("Você ganhou!")
+            placar[1] += 1
+
+        # Pedra Tesoura
+        elif cliente_choice == 3 and server_choice == 1:
+            print("\n\n Você: Pedra")
+            print("Oponente: Tesoura")
+            print("Você ganhou!")
+            placar[1] += 1
+
+        # Papel Tesoura
+        elif cliente_choice == 3 and server_choice == 2:
+            print("\n\n Você: Papel")
+            print("Oponente: Tesoura")
+            print("Você perdeu!")
+            placar[0] += 1
+
+        # Tesoura Tesoura
+        elif cliente_choice == 3 and server_choice == 3:
+            print("\n\n Você: Tesoura")
+            print("Oponente: Tesoura")
+            print("Empate")
+
         else:
+            print("Houve algum erro")
+            print("Encerrando conexão.....")
             break
 conn.close()
 
